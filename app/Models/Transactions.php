dd7644f6a3407896f712b382f2418b96f3c81614
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-22 14:57:40 -0500

    Add factory support and relationships to Transactions model

--- app/Models/Transactions.php
+++ app/Models/Transactions.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transactions extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['amount', 'description', 'team_id'];
+
+    public function team(){
+        return $this->belongsTo(Team::class);
+    }
 }
