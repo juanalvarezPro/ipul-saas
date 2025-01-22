5242481824d735360303643878263ced22e73b46
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-22 14:57:30 -0500

    Add Team model relationships and enable mass assignment

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    use HasFactory;
+    protected $guarded = [];
+
+    
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'team_user', 'team_id', 'user_id');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transactions::class);
+    }
 }
