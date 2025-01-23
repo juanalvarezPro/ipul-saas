5b62532da0a557660d96095872bdf3dff9a57a9e
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-22 19:26:18 -0500

    Add Transactions resource with CRUD pages and form schema

---
+++ app/Filament/Resources/TransactionsResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionsResource\Pages;
+use App\FilaTment\Resources\TransactionsResource\RelationManagers;
+use App\Models\Transactions;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransactionsResource extends Resource
+{
+    protected static ?string $model = Transactions::class;
+    protected static ?string $tenantRelationshipName ='transactions';
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('amount')
+                    ->label('Amount')
+                    ->numeric()
+                    ->required(),
+                Forms\Components\TextInput::make('description')
+                    ->label('Description')
+                    ->required(),
+                Forms\Components\TextInput::make('team_id')
+                    ->label('Team ID')
+                    ->numeric()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('amount')
+                ->label('Amount')
+                ->sortable()
+                ->searchable(),
+            Tables\Columns\TextColumn::make('description')
+                ->label('Description')
+                ->sortable()
+                ->searchable(),
+            Tables\Columns\TextColumn::make('team_id')
+                ->label('Team ID')
+                ->sortable()
+                ->searchable(),
+        ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransactions::route('/create'),
+            'edit' => Pages\EditTransactions::route('/{record}/edit'),
+        ];
+    }
+}
