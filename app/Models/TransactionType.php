c0d193330ef68cfbe3832804000b81765db5fbf7
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-22 22:52:54 -0500

    Add TransactionType resource with CRUD pages and model implementation

---
+++ app/Models/TransactionType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionType extends Model
+{
+    public function transactions()
+    {
+        return $this->hasMany(Transactions::class, 'concept_id');
+    }
+}
