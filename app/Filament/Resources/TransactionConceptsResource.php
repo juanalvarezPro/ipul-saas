01ed176a917ad67b17c53dc88cf2a6bc39588129
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-26 21:04:11 -0500

    Enhance TransactionConceptsResource with improved labels, navigation, and form structure for better user experience

--- app/Filament/Resources/TransactionConceptsResource.php
+++ app/Filament/Resources/TransactionConceptsResource.php
@@ -12,27 +12,40 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Facades\Filament;
 
 class TransactionConceptsResource extends Resource
 {
     protected static ?string $model = TransactionConcepts::class;
     protected static ?string $tenantOwnershipRelationshipName = 'team';
     protected static ?string $tenantRelationshipName = 'transactionConcepts';
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-square-3-stack-3d';
+    protected static ?string $navigationLabel = 'Conceptos';
+    protected static ?string $navigationGroup = 'Gestión del Sistema';
+    protected static ?int $navigationSort = 2;
+    protected static ?string $modelLabel = "Concepto";
+
 
     public static function form(Form $form): Form
     {
         return $form
-        ->schema([
-            Forms\Components\TextInput::make('name'),
-            Forms\Components\Toggle::make('active')
-            ->label('Active')
-            ->default(true),
-            Forms\Components\TextInput::make('description'),
-            Forms\Components\Select::make('transaction_type_id')
-            ->relationship('transactionType', 'name') 
-            ->required(),
-        ]);
+            ->schema([
+                Forms\Components\TextInput::make('name')->label('Nombre')->required(),
+                Forms\Components\TextInput::make('description')->label('Descripción'),
+                Forms\Components\Select::make('transaction_type_id')
+                    ->label('Movimiento')
+                    ->relationship(
+                        'transactionType',
+                        'name',
+                        fn(Builder $query) => $query->whereBelongsTo(Filament::getTenant())->where('active', true)
+                    )
+                    ->searchable()
+                    ->preload()
+                    ->required()
+                    ->live()
+            ])
+            ->columns(3)
+        ;
     }
 
     public static function table(Table $table): Table
@@ -40,25 +53,28 @@ class TransactionConceptsResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
-                ->label('name')
-                ->sortable()
-                ->searchable(),
+                    ->label('Nombre')
+                    ->sortable()
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('description')
-                ->label('description')
-                ->sortable()
-                ->searchable(),
+                    ->label('Descripción')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('transactionType.name')
-                ->label('Transaction Type Name')
-                ->sortable()
-                ->searchable(),
-                Tables\Columns\ToggleColumn::make('active')
+                    ->label('Movimiento')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\ToggleColumn::make('active')->label('Activo')
                 
+
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
