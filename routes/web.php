a3c7ebdd9c89415c59c6ae70fe87dbaed69abe21
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-02-08 00:26:58 -0500

    Update routes to redirect to /app and comment out unused routes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SocialLoginController;
 use App\Http\Controllers\SessionRevocationController;
 
-
-Route::get('/logout-session/{session_id}', [SessionRevocationController::class, 'revoke'])
-    ->name('logout.session');
-
-Route::get('/auth/google', [SocialLoginController::class, 'redirectToProvider'])->name('auth.google');
-Route::get('/auth/google/callback', [SocialLoginController::class, 'socialCallback']);
-
-Route::fallback(function () {
+Route::get('/', function () {
     return redirect('/app');
 });
+
+// Route::get('/logout-session/{session_id}', [SessionRevocationController::class, 'revoke'])
+//     ->name('logout.session');
+
+// Route::get('/auth/google', [SocialLoginController::class, 'redirectToProvider'])->name('auth.google');
+// Route::get('/auth/google/callback', [SocialLoginController::class, 'socialCallback']);
+
