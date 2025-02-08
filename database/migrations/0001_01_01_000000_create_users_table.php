5c00ee021825bf64921d222ad34a768156b6e21d
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-02-07 23:00:47 -0500

    Add nullable email_personal column to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('email_personal')->unique()->nullable();
             $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
