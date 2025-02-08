a87c85e9aa67248f92ed9cf87684335eb75278a3
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-02-07 21:57:57 -0500

    Add is_admin column to users table and remove active column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('status', ['pending', 'approved'])->default('pending');
-            $table->boolean('active')->default(true);
             $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
