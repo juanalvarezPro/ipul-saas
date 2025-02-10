4dcbf688ee93160dc36042f1d2e0854b7d89b062
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-02-09 21:22:21 -0500

    Add migrations for countries, states, and cities; update churches table with foreign keys

--- database/migrations/2025_01_22_160348_create_churches_table.php
+++ database/migrations/2025_01_22_160348_create_churches_table.php
@@ -14,13 +14,23 @@ return new class extends Migration
         Schema::create('churches', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('address')->nullable(false);
+            $table->unsignedBigInteger('country_id')->default(170);
+            $table->unsignedBigInteger('Province_id');
+            $table->unsignedBigInteger('corregimiento_id');
+            $table->string('pastor_name'); 
+            $table->string('email')->nullable();
+            $table->string('phone'); 
+            $table->string('street_address');
             $table->boolean('active')->default(true);
             $table->timestamps();
 
+            // Agregar claves foráneas
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
+            $table->foreign('province_id')->references('id')->on('states')->onDelete('cascade');
+            $table->foreign('corregimiento_id')->references('id')->on('cities')->onDelete('cascade');
         });
     }
-    
+
 
     /**
      * Reverse the migrations.
