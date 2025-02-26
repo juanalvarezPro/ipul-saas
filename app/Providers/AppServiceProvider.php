2878cef9a1b44d576cfef44239b6c16e20ef991f
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-02-26 16:03:25 -0500

    add Panel Switch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Services\Contracts\SocialAuthProviderInterface;
 use App\Services\GoogleAuthService;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use BezhanSalleh\PanelSwitch\PanelSwitch;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,25 @@ class AppServiceProvider extends ServiceProvider
       if (app()->environment('production')) {
           URL::forceScheme('https');
       }
+      PanelSwitch::configureUsing(function (PanelSwitch $panelSwitch) {
+        // Custom configurations go here
+        $panelSwitch
+        ->panels(['admin', 'app'])
+        ->modalWidth('sm')
+        ->slideOver()
+        ->modalHeading('Paneles Disponibles')
+        ->visible(fn (): bool => auth()->user()?->hasAnyRole([
+            'super_admin',
+        ]))
+        ->icons([
+          'admin' => 'heroicon-o-square-2-stack',
+          'app' => 'heroicon-o-star',
+      ])
+        ->iconSize(16)
+        ->labels([
+            'admin' => 'Administrador',
+            'app' => 'Personal'
+        ]);
+    });
   }
 }
